31b68bcede874e6e61d80ae71d78246f1dc3f941
Fernando Caldera <fer.caldera@gmail.com>
2024-04-09 01:23:18 +0300

    Create install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# install.sh
+# symlink my dotfiles to appropriate places
+
+DIR=$HOME/dotfiles
+
+DOTFILES=(
+	# ".bin"
+	# ".bashrc"
+	# ".bash_profile"
+	".gitconfig"
+	# ".profile"
+	".tmux.conf"
+	# ".xinitrc"
+	# ".config/cmus/cmus.theme"
+	# ".config/compton.conf"
+	# ".config/dunst"
+	# ".config/feh"
+	# ".config/mpv"
+	".config/nvim"
+	# ".local/share/fonts"
+)
+
+for dotfile in "${DOTFILES[@]}";do
+	rm -rf "${HOME}/${dotfile}"
+	ln -sf "${DIR}/${dotfile}" "${HOME}/${dotfile}"
+done
