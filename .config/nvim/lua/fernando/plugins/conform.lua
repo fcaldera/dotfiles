0dfddb1ee624b4894b811ec5cb9807b5fb9a820e
Fernando Caldera <fer.caldera@gmail.com>
2024-04-13 11:35:26 +0300

    Enable javascript formatter

--- .config/nvim/lua/fernando/plugins/conform.lua
+++ .config/nvim/lua/fernando/plugins/conform.lua
@@ -25,12 +25,12 @@ return { -- Autoformat
     -- end,
     formatters_by_ft = {
       lua = { "stylua" },
+      javascript = { { "prettierd", "prettier" } },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
     },
   },
 }
