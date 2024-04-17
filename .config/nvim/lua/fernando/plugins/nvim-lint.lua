5d3dd9154b44d8a1ae5dd714f0ae63cd09b884eb
Fernando Caldera <fer.caldera@gmail.com>
2024-04-18 01:03:16 +0300

    Reconfigure the linter

--- .config/nvim/lua/fernando/plugins/nvim-lint.lua
+++ .config/nvim/lua/fernando/plugins/nvim-lint.lua
@@ -1,21 +1,19 @@
 return {
   "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("lint").linters_by_ft = {
       javascript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
-      markdown = { "vale" },
     }
 
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    -- Create autocommand which carries out the actual linting
+    -- on the specified events.
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
       callback = function()
-        -- try_lint without arguments runs the linters defined in `linters_by_ft`
-        -- for the current filetype
         require("lint").try_lint()
-
-        -- You can call `try_lint` with a linter name or a list of names to always
-        -- run specific linters, independent of the `linters_by_ft` configuration
-        -- require("lint").try_lint("cspell")
       end,
     })
   end,
