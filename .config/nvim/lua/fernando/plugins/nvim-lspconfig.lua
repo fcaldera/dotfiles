ae12b0c3e440e71932e42f157c8ca3db1ea7527b
Fernando Caldera <fer.caldera@gmail.com>
2024-04-21 01:30:08 +0300

    Enable Mason config. (from kickstart)

--- .config/nvim/lua/fernando/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/fernando/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@ return { -- LSP Configuration & Plugins
   "neovim/nvim-lspconfig",
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for Neovim
-    "williamboman/mason.nvim",
+    { "williamboman/mason.nvim", config = true },
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
 
