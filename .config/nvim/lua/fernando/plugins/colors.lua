f0a8a4a6247c197ce0e6f79cba7bb11bb8befb3d
Fernando Caldera <fer.caldera@gmail.com>
2024-04-28 20:40:53 +0300

    dim inactive panels

--- .config/nvim/lua/fernando/plugins/colors.lua
+++ .config/nvim/lua/fernando/plugins/colors.lua
@@ -11,6 +11,13 @@ local catppuccin = {
         dark = "mocha",
       },
       transparent_background = false,
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "light",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
       integrations = {
         cmp = true,
         gitsigns = true,
