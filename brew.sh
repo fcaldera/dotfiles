8fa548d85afb43632368393b80169b9cde6fb9bf
Fernando Caldera <fer.caldera@gmail.com>
2024-05-07 20:00:07 +0300

    Add brew.sh

---
+++ brew.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install command-line and apps tools using Homebrew.
+# https://github.com/mathiasbynens/dotfiles/blob/main/brew.sh
+
+brew install trash
+brew install jq
+brew install tree
+brew install gh
+brew install nano
+brew install nvim
+brew install tmux
+brew install ripgrep
+brew install entr
+
+
+# Fonts
+brew tap homebrew/cask-fonts
+brew install font-monaspace
+brew install font-caskaydia-cove-nerd-font
