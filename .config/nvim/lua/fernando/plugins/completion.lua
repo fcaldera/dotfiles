3534edfe9fa85da8cc5a1e998e6b46602fa74480
Fernando Caldera <fer.caldera@gmail.com>
2024-05-25 01:18:21 +0300

    Improve completions experience

--- .config/nvim/lua/fernando/plugins/completion.lua
+++ .config/nvim/lua/fernando/plugins/completion.lua
@@ -1,7 +1,6 @@
 return {
   "hrsh7th/nvim-cmp",
-  lazy = false,
-  priority = 100,
+  event = "InsertEnter",
   dependencies = {
     "onsails/lspkind.nvim",
     "hrsh7th/cmp-nvim-lsp",
@@ -11,52 +10,62 @@ return {
     "saadparwaiz1/cmp_luasnip",
   },
   config = function()
-    vim.opt.completeopt = { "menu", "menuone", "noinsert" }
     vim.opt.shortmess:append("c")
 
     local cmp = require("cmp")
-
     local luasnip = require("luasnip")
-    luasnip.config.setup({
-      update_events = { "TextChanged", "TextChangedI" },
-    })
+    luasnip.config.setup({})
 
     local lspkind = require("lspkind")
     lspkind.init({})
 
     cmp.setup({
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-        { name = "buffer" },
-      },
-      mapping = {
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-
-        -- Accept ([y]es) the completion.
-        ["<C-y>"] = cmp.mapping(
-          cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-          }),
-          { "i", "c" }
-        ),
-
-        -- Abort completion
-        ["<C-e>"] = cmp.mapping.abort(),
-
-        -- Manually trigger a completion from nvim-cmp.
-        ["<C-Space>"] = cmp.mapping.complete({}),
-      },
-
       -- Enable luasnip to handle snippet expansion for nvim-cmp
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
+
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer" },
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+        -- Scroll the documentation window [b]ack / [f]orward
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+        -- Accept ([y]es) the completion.
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+        -- Manually trigger a completion from nvim-cmp.
+        ["<C-Space>"] = cmp.mapping.complete({}),
+
+        -- <c-l> will move you to the right of each of the expansion locations.
+        ["<C-l>"] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { "i", "s" }),
+
+        -- <c-h> is similar, except moving you backwards.
+        ["<C-h>"] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { "i", "s" }),
+      }),
 
       ---@diagnostic disable-next-line missing-fields
       formatting = {
@@ -66,6 +75,12 @@ return {
           ellipsis_char = "...",
           show_labelDetails = true,
         }),
+      },
+
+      -- Add borders to completions popups
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
     })
 
@@ -81,19 +96,5 @@ return {
     for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/fernando/snippets/*.lua", true)) do
       loadfile(ft_path)()
     end
-
-    -- <c-l> will move you to the right of each of the expansion locations.
-    vim.keymap.set({ "i", "s" }, "<c-l>", function()
-      if luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      end
-    end, { silent = true })
-
-    -- <c-h> is similar, except moving you backwards.
-    vim.keymap.set({ "i", "s" }, "<c-h>", function()
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      end
-    end, { silent = true })
   end,
 }
