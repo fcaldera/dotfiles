457c576df82f083e4ecfe7ca7065a96ecd08fa20
Fernando Caldera <fer.caldera@gmail.com>
2024-05-25 03:24:31 +0300

    Review LSP configuration one more time

--- .config/nvim/lua/fernando/plugins/lsp.lua
+++ .config/nvim/lua/fernando/plugins/lsp.lua
@@ -16,27 +16,110 @@ return {
     "b0o/SchemaStore.nvim",
   },
   config = function()
-    require("neodev").setup()
+    local disable_semantic_tokens = {
+      lua = true,
+    }
 
-    local capabilities = nil
-    if pcall(require, "cmp_nvim_lsp") then
-      capabilities = require("cmp_nvim_lsp").default_capabilities()
-    end
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc)
+          vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
 
-    local lspconfig = require("lspconfig")
+        -- To jump back, press <C-t>.
+        map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+        map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        map("gT", vim.lsp.buf.type_definition, "[T]ype Definition")
+
+        map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+        map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+        map("<leader>cr", vim.lsp.buf.rename, "[C]ode [R]ename")
+        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+        map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+        -- The following two autocommands are used to highlight references of the
+        -- word under your cursor when your cursor rests there for a little while.
+        --    See `:help CursorHold` for information about when this is executed
+        --
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+          local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
+
+          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
+
+          vim.api.nvim_create_autocmd("LspDetach", {
+            group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+            end,
+          })
+        end
+
+        -- The following autocommand is used to enable inlay hints in your
+        -- code, if the language server you are using supports them
+        --
+        -- This may be unwanted, since they displace some of your code
+        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+          map("<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+          end, "[T]oggle Inlay [H]ints")
+        end
+
+        local filetype = vim.bo[event.buf].filetype
+        if disable_semantic_tokens[filetype] then
+          client.server_capabilities.semanticTokensProvider = nil
+        end
+      end,
+    })
+
+    -- Servers configuration
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
     local servers = {
-      lua_ls = true,
-      cssls = true,
-      html = true,
-      -- bashls = true,
-      -- gopls = false,
-      -- rust_analyzer = false,
-      -- svelte = false,
-      -- templ = false,
+      lua_ls = {
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = "Replace",
+            },
+            diagnostics = {
+              globals = { "vim" },
+              disable = { "missing-fields" },
+            },
+          },
+        },
+      },
+      cssls = {},
+      html = {},
 
       -- Probably want to disable formatting for this lang server
-      tsserver = true,
+      tsserver = {
+        capabilities = {
+          textDocument = {
+            formatting = {
+              dynamicRegistration = false,
+              documentFormatting = false,
+            },
+          },
+        },
+      },
 
       volar = {
         filetypes = { "vue", "javascript", "typescript" },
@@ -67,105 +150,40 @@ return {
         cmd = { "/Users/fernando/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
         root_dir = require("lspconfig.util").root_pattern({ "mix.exs" }),
       },
-
-      -- lexical = {
-      --   cmd = { "/Users/fernando/.local/share/nvim/mason/packages/lexical/lexical", "server" },
-      --   root_dir = require("lspconfig.util").root_pattern({ "mix.exs" }),
-      -- },
     }
 
-    local servers_to_install = vim.tbl_filter(function(key)
-      local t = servers[key]
-      if type(t) == "table" then
-        return not t.manual_install
-      else
-        return t
-      end
-    end, vim.tbl_keys(servers))
-
+    -- Servers instalation
     require("mason").setup()
+    local ensure_installed = vim.tbl_keys(servers or {})
 
-    local ensure_installed = {
+    vim.list_extend(ensure_installed, {
       "stylua",
       "prettierd",
       "eslint_d",
-    }
+    })
 
-    vim.list_extend(ensure_installed, servers_to_install)
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-    for name, config in pairs(servers) do
-      if config == true then
-        ---@diagnostic disable-next-line cast-local-type
-        config = {}
-      end
-      ---@diagnostic disable-next-line cast-local-type
-      config = vim.tbl_deep_extend("force", {}, {
-        capabilities = capabilities,
-      }, config)
-
-      lspconfig[name].setup(config)
-    end
-
-    local disable_semantic_tokens = {
-      lua = true,
-    }
-
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-        local bufnr = args.buf
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
-
-        vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0, desc = "LSP: [G]oto [D]efinition" })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0, desc = "LSP: [G]oto [R]eferences" })
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0, desc = "LSP: [G]oto [D]eclaration" })
-        vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0, desc = "LSP: [T]ype Definition" })
-
-        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0, desc = "LSP: [C]ode [R]ename" })
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0, desc = "LSP: [C]ode [A]action" })
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        vim.keymap.set(
-          "n",
-          "<leader>ds",
-          require("telescope.builtin").lsp_document_symbols,
-          { buffer = bufnr, desc = "[D]ocument [S]ymbols" }
-        )
-
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        vim.keymap.set(
-          "n",
-          "<leader>ws",
-          require("telescope.builtin").lsp_dynamic_workspace_symbols,
-          { buffer = bufnr, desc = "[W]orkspace [S]ymbols" }
-        )
-
-        local filetype = vim.bo[bufnr].filetype
-        if disable_semantic_tokens[filetype] then
-          client.server_capabilities.semanticTokensProvider = nil
-        end
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        if client and client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            buffer = args.buf,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-            buffer = args.buf,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end,
+    require("mason-lspconfig").setup({
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          -- This handles overriding only values explicitly passed
+          -- by the server configuration above. Useful when disabling
+          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+          require("lspconfig")[server_name].setup(server)
+        end,
+      },
     })
+
+    -- Globally configure all LSP floating preview popups (like hover, signature help, etc)
+    local open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = opts.border or "rounded" -- Set border to rounded
+      return open_floating_preview(contents, syntax, opts, ...)
+    end
 
     -- Autoformatting Setup
     require("conform").setup({
@@ -195,3 +213,5 @@ return {
     })
   end,
 }
+
+-- vim: ts=2 sts=2 sw=2 et
