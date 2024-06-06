90f10d63dd0023a3745549b8af17b7edc4bb8e84
Fernando Caldera <fer.caldera@gmail.com>
2024-06-06 21:07:44 +0300

    Refactor treesitter configuration

--- .config/nvim/lua/fernando/plugins/treesitter.lua
+++ .config/nvim/lua/fernando/plugins/treesitter.lua
@@ -3,30 +3,15 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.install").prefer_git = true
-    local group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true })
 
     ---@diagnostic disable-next-line missing-fields
     require("nvim-treesitter.configs").setup({
       ensure_installed = { "diff", "elixir" },
-      auto_install = false,
+      auto_install = true,
       highlight = { enable = true },
-    })
-
-    local syntax_on = {
-      elixir = true,
-    }
-
-    vim.api.nvim_create_autocmd("FileType", {
-      group = group,
-      callback = function(args)
-        local bufnr = args.buf
-        local ft = vim.bo[bufnr].filetype
-        pcall(vim.treesitter.start)
-
-        if syntax_on[ft] then
-          vim.bo[bufnr].syntax = "on"
-        end
-      end,
+      indent = { enable = true },
     })
   end,
 }
+
+-- vim: ts=2 sts=2 sw=2 et
