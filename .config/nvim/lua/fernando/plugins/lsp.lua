b546e49f76d98bdf1d326482eb5f01702532323a
Fernando Caldera <fer.caldera@gmail.com>
2024-06-10 12:50:01 +0300

    Update statusline

--- .config/nvim/lua/fernando/plugins/lsp.lua
+++ .config/nvim/lua/fernando/plugins/lsp.lua
@@ -214,6 +214,13 @@ return {
         })
       end,
     })
+
+    -- Change diagnostic symbols in the sign column (gutter)
+    local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
   end,
 }
 
--- .config/nvim/lua/fernando/plugins/mini.lua
+++ .config/nvim/lua/fernando/plugins/mini.lua
@@ -4,35 +4,18 @@ return {
     require("mini.ai").setup()
     require("mini.surround").setup()
 
-    require("mini.statusline").setup({
-      use_icons = vim.g.have_nerd_font,
-      content = {
-        active = function()
-          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-          local git = MiniStatusline.section_git({ trunc_width = 75 })
-          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-          local location = "%2l:%-2v" -- MiniStatusline.section_location({ trunc_width = 75 })
-          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-          local copilot = ""
+    local statusline = require("mini.statusline")
+    statusline.setup({ use_icons = vim.g.have_nerd_font })
 
-          local ok, clients = pcall(vim.lsp.get_active_clients, { name = "GitHub Copilot", bufnr = 0 })
-          if ok and #clients > 0 then
-            copilot = vim.g.copilot_enabled and " " or " "
-          end
+    statusline.section_location = function()
+      return "%2l:%-2v"
+    end
 
-          return MiniStatusline.combine_groups({
-            { hl = mode_hl, strings = { string.upper(mode) } },
-            { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics, copilot } },
-            "%<", -- Mark general truncate point
-            { hl = "MiniStatuslineFilename", strings = { filename } },
-            "%=", -- End left alignment
-            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-            { hl = mode_hl, strings = { search, location } },
-          })
-        end,
-      },
-    })
+    local orig_sm = statusline.section_mode
+
+    statusline.section_mode = function(args)
+      local mode, mode_hl = orig_sm(args)
+      return string.upper(mode), mode_hl
+    end
   end,
 }
