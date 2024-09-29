b25c8b2bf4639222795efe9f7f6e141df292f23f
Fernando Caldera <fer.caldera@gmail.com>
2024-09-29 22:40:15 +0300

    Upgrade which-key v3
    Fall upgrade from kick-start

--- .config/nvim/lua/fernando/plugins/which-key.lua
+++ .config/nvim/lua/fernando/plugins/which-key.lua
@@ -1,21 +1,22 @@
 return {
   "folke/which-key.nvim",
   event = "VimEnter",
-  config = function()
-    require("which-key").setup()
-
-    require("which-key").register({
-      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-      ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-      ["<leader>r"] = { name = "[R]eplace", _ = "which_key_ignore" },
-      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-      ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-      ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-      ["<leader>b"] = { name = "[B]uffers", _ = "which_key_ignore" },
-      ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-      ["<leader>v"] = { name = "[V]iew", _ = "which_key_ignore" },
-      ["<leader>q"] = { name = "[Q]uickfix", _ = "which_key_ignore" },
-      ["<leader>k"] = { name = "Commands", _ = "which_key_ignore" },
-    })
-  end,
+  opts = {
+    icons = {
+      mappings = false,
+    },
+    spec = {
+      { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>r", group = "[R]eplace" },
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>w", group = "[W]orkspace" },
+      { "<leader>g", group = "[G]it" },
+      { "<leader>b", group = "[B]uffers" },
+      { "<leader>t", group = "[T]oggle" },
+      { "<leader>v", group = "[V]iew" },
+      { "<leader>q", group = "[Q]uickfix" },
+      { "<leader>k", group = "Commands" },
+    },
+  },
 }
