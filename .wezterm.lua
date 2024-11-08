b35ab4975463c4cf26b6a218d21eeac60bd0246c
Fernando Caldera <fer.caldera@gmail.com>
2024-11-08 23:57:38 +0200

    Wezterm: configure typography

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,11 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.95
 
+config.font = wezterm.font("Monaspace Krypton")
+config.harfbuzz_features = { "calt", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "ss09", "liga" }
 config.font_size = 14
+config.line_height = 1.1
+
 config.color_scheme = "rose-pine"
 
 return config
