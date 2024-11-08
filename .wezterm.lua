4fb6bfc473908096bd5a45dd10525640a3fd3492
Fernando Caldera <fer.caldera@gmail.com>
2024-11-08 21:14:52 +0200

    Improve gradients and background transparencies for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,40 +4,21 @@ local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
 
 config.font_size = 14
 config.color_scheme = "rose-pine"
 
 local scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
 
-local gradients = {
-	ltr = {
-		colors = {
-			scheme.background,
-			scheme.background,
-			scheme.ansi[1],
-		},
+config.window_background_gradient = {
+	colors = {
+		scheme.background,
+		scheme.background,
+		scheme.ansi[1],
+		scheme.background,
 	},
-
-	diagonal = {
-		colors = {
-			scheme.background,
-			scheme.background,
-			scheme.ansi[1],
-			scheme.background,
-		},
-		orientation = { Linear = { angle = 45.0 } },
-	},
-
-	radial = {
-		colors = {
-			scheme.ansi[1],
-			scheme.background,
-		},
-		orientation = { Radial = { cx = 1.0, cy = 0.0, radius = 0.75 } },
-	},
+	orientation = { Linear = { angle = 45.0 } },
 }
-
-config.window_background_gradient = gradients["diagonal"]
 
 return config
