cbb515a3445747c0da52ba83fca7bacb2c9af1ca
Fernando Caldera <fer.caldera@gmail.com>
2024-12-13 23:55:19 +0200

    Add telescope multigrep and package search

--- .config/nvim/lua/fernando/plugins/telescope.lua
+++ .config/nvim/lua/fernando/plugins/telescope.lua
@@ -40,12 +40,13 @@ return {
 
     -- See `:help telescope.builtin`
     local builtin = require("telescope.builtin")
+    local custom = require("fernando.custom.telescope")
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
     vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sg", custom.live_multigrep, { desc = "[S]earch by [G]rep" })
     vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
     vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
     vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -73,6 +74,13 @@ return {
     vim.keymap.set("n", "<leader>sn", function()
       builtin.find_files({ cwd = vim.fn.stdpath("config") })
     end, { desc = "[S]earch [N]eovim files" })
+
+    -- Shortcut for searching packages files
+    vim.keymap.set("n", "<leader>sp", function()
+      builtin.find_files({
+        cwd = vim.fs.joinpath(tostring(vim.fn.stdpath("data")), "lazy"),
+      })
+    end, { desc = "[S]earch [P]ackages files" })
 
     -- stylua: ignore
     local builtin_colors = {
