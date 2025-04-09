769a82bddb7b6d296288b6a00b57db907dfc30fc
Fernando Caldera <fer.caldera@gmail.com>
2025-04-09 07:13:14 +0300

    Use vim-surround

--- .config/nvim/lua/fernando/plugins/init.lua
+++ .config/nvim/lua/fernando/plugins/init.lua
@@ -2,5 +2,6 @@ return {
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
   "tpope/vim-fugitive", -- Git integration
   "tpope/vim-rhubarb", -- GH integration
+  "tpope/vim-surround",
   "mbbill/undotree",
 }
