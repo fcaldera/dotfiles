941097fcfb47e6cdf2e31a1ec7ae600a9574a585
Fernando Caldera <fer.caldera@gmail.com>
2025-10-25 19:18:59 +0300

    feat: best navigation keymap ever

--- .config/nvim/lua/fernando/keymaps.lua
+++ .config/nvim/lua/fernando/keymaps.lua
@@ -5,10 +5,6 @@ vim.opt.hlsearch = true
 set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Split window management
-set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 set("n", "<leader>x", "<C-w>c", { desc = "Close the current window" })
 
 -- Control the size of splits (height/width)
