68fbe4787420bed3a9c3c1de80325d0b59c1f5df
Fernando Caldera <fer.caldera@gmail.com>
2025-11-28 02:34:07 +0200

    nvim: enable html snippets in jsx

--- .config/nvim/lua/fernando/plugins/completion.lua
+++ .config/nvim/lua/fernando/plugins/completion.lua
@@ -16,6 +16,8 @@ return {
           "rafamadriz/friendly-snippets",
           config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip").filetype_extend("javascriptreact", { "html" })
+            require("luasnip").filetype_extend("typescriptreact", { "html" })
           end,
         },
       },
