a73d27267920dc12a8473309b6591979117423fd
Fernando Caldera <fer.caldera@gmail.com>
2025-12-05 22:15:28 +0200

    nvim: improve typescript sql language injections

--- .config/nvim/queries/typescript/injections.scm
+++ .config/nvim/queries/typescript/injections.scm
@@ -1,7 +1,19 @@
 ;; extends
 
 (call_expression
-  function: (identifier) @func_name (#eq? @func_name "sql")
+  function: [
+    ; Direct identifier case
+    (identifier) @func_name
+    
+    ; Nested expression case
+    (non_null_expression
+      (instantiation_expression
+        (await_expression
+          (identifier) @func_name)))
+  ]
+  (#eq? @func_name "sql")
   arguments: (template_string) @injection.content
-  (#set! injection.language "sql"))
-
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "sql")
+  (#set! injection.combined)
+  (#set! injection.include-children))
