a3bf55b5ec919f194b6bec244d2717805fb5ef0d
Fernando Caldera <fer.caldera@gmail.com>
2025-12-13 20:34:38 +0200

    chore: restore main sudo -v access

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 if ! command -v brew >/dev/null 2>&1; then
 	echo "Installing Homebrew..."
+	sudo -v
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	eval "$(/opt/homebrew/bin/brew shellenv)"
 else
